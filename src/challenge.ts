f3aa287279100a08908285a2da2980bf6281aec1
TariqBazadough <tariq.bazadough@gmail.com>
2025-02-28 23:50:59 +0300

    I have changed the following: 1- function description 2- added function return example

--- src/challenge.ts
+++ src/challenge.ts
@@ -1,28 +1,36 @@
+// don't remove this import line
 import { Book } from "./objects";
 
+// don't change this interface
 interface Review {
   reviewer: string;
   comment: string;
 }
 
+// don't change this interface
 interface ReviewedBook extends Book {
   reviews?: Review[];
 }
 
 /**
- * 🌶️🌶️🌶️ addReview
+ * 1) 🌶️🌶️🌶️ Using `addReview` function:
+ * - Accepts 3 parameters:
+ *   - A `book` parameter of type `ReviewedBook` (which may or may not have a `reviews` property).
+ *   - A `reviewer` parameter of type `string`.
+ *   - A `comment` parameter of type `string`.
+ * - Creates a new review object `{ reviewer, comment }` and adds it to the book's `reviews` array.
+ * - If `reviews` does not exist, it should initialize it as an empty array before adding the review.
+ * - Returns the updated book object with the new review included in the `reviews` array.
  *
- * - receives a book object which MIGHT have a reviews property
- * - receives a reviewer
- * - receives a comment
- *
- * Create a new review object made up of a `reviewer` and `comment`
- *  and add it to the book's reviews array
- *
- * - returns the book object with the new review included in the reviews array
+ * Example:
+ * const book = { title: "1984", author: "George Orwell", publishedYear: 1949, genre: "Dystopian" };
+ * addReview(book, "Alice", "A thought-provoking novel!");
+ * // => { title: "1984", author: "George Orwell", publishedYear: 1949, genre: "Dystopian", reviews: [{ reviewer: "Alice", comment: "A thought-provoking novel!" }] }
  */
-function addReview(book: ReviewedBook, reviewer: string, comment: string) {
-  // write your code here
+function addReview(book: ReviewedBook, reviewer: string, comment: string): ReviewedBook {
+  // write your code here...
+
+  return book;
 }
 
 export { addReview, Review, ReviewedBook };
